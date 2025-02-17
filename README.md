214e41a260d9de39bb4aaca4d00363831bff76a5
Jungkoo Kang <jungkoo.kang@ibm.com>
2025-02-17 09:34:29 -0500

    Make the project a package

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # Experiment
+
+### Build API-Integrated-LLM Package
+
+Start with creating a new virtual environment.
+
+```bash
+conda create --name ail python=3.10 -y
+conda activate ail
+```
+
+The tools in `agent-gym` can be conveniently utilized as Python packages. To build a local package, clone the repor and execute the following commands 
+from the project's root directory. 
+
+```bash
+git clone git@github.ibm.com:Jungkoo-Kang/api_integrated_llm_experiment.git
+cd api_integrated_llm_experiment
+```
+
+Now, to build, run:
+
+```bash
+pip install -e .
+python -m pip install --upgrade build
+python -m build
+```
+
+## Installation Guide
+
+To install the CLI, follow these steps:
+
+1. Navigate to your Python virtual environment.
+2. Run the following command to install the package from the built WHL file:
+
+```bash
+pip install <PATH_TO_BUILT_WHL_FILE>
+```
+
+3. To verify that the installation was successful, execute the following command to display the help menu:
+```bash
+api-integrated-llm -h
+```
+
+## Command Line Interface (CLI)
+
+The source folder should have the following structure:
+
+```txt
+.
+├── source
+│   ├── evaluation
+│   │   └── <YOUR_DATA_HERE>
+│   └── prompts
+│       ├── examples_icl.json
+│       └── prompts.json
+```
+
+To evaluate and score data, use the following command:
+
+```bash
+api-integrated-llm -rt <PATH_TO_DATA_SOURCE_FOLDER>
+```
---
+++ api_integrated_llm/helpers/cli_helper.py
@@ -0,0 +1,62 @@
+import argparse
+from pathlib import Path
+import os
+from api_integrated_llm.evaluation import evaluate
+from api_integrated_llm.helpers.benchmark_helper import get_model_id_obj_dict
+from api_integrated_llm.helpers.file_helper import get_files_in_folder
+from api_integrated_llm.scoring import scoring
+
+
+def get_arguments() -> argparse.Namespace:
+    project_root_path = Path(__file__).parent.parent.parent.resolve()
+    parser = argparse.ArgumentParser(description="Conversational AI Gym Tool")
+
+    # Common arguments
+    parser.add_argument(
+        "-rt",
+        "--root",
+        type=str,
+        help="Dataset root absolute path",
+        default=str(project_root_path),
+    )
+
+    return parser.parse_args()
+
+
+def cli() -> None:
+    args = get_arguments()
+    source_folder_path = os.path.join(args.root, "source")
+    output_folder_path = os.path.join(args.root, "output")
+    evaluation_folder_path = os.path.join(
+        output_folder_path,
+        "evaluation",
+    )
+
+    evaluate(
+        model_id_info_dict=get_model_id_obj_dict(),
+        evaluation_input_file_paths=get_files_in_folder(
+            folder_path=os.path.join(source_folder_path, "evaluation"),
+            file_extension="json",
+        ),
+        example_file_path=os.path.join(
+            source_folder_path, "prompts", "examples_icl.json"
+        ),
+        output_folder_path=evaluation_folder_path,
+        prompt_file_path=os.path.join(source_folder_path, "prompts", "prompts.json"),
+        error_folder_path=os.path.join(
+            output_folder_path,
+            "error",
+        ),
+        temperatures=[0.0],
+        max_tokens_list=[1500],
+        should_generate_random_example=True,
+        num_examples=3,
+    )
+    scoring(
+        evaluator_output_file_paths=get_files_in_folder(
+            folder_path=evaluation_folder_path,
+            file_extension="jsonl",
+        ),
+        output_folder_path=os.path.join(output_folder_path, "scoring"),
+        win_rate_flag=False,
+    )
