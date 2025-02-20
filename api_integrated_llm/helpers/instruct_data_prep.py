b0a7b10480d612702ce0602ea31288360f5805db
Jungkoo Kang <jungkoo.kang@ibm.com>
2025-02-20 11:25:15 -0500

    Change default prompt for rest datasets

--- api_integrated_llm/helpers/instruct_data_prep.py
+++ api_integrated_llm/helpers/instruct_data_prep.py
@@ -58,7 +58,7 @@ def get_prompt_dict(
     prompt_dict_all = get_dict_from_json(file_path=prompt_file_path)  # type: ignore
     path_str = str(evaluation_input_file_path)
     if "rest" in path_str:
-        return prompt_dict_all["router"]
+        return prompt_dict_all["icl"]
     if "sequencing" in path_str:
         return prompt_dict_all["sequencing"]
     return prompt_dict_all["icl"]
