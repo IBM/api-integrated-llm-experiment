e91484f3717e4989fe12c81aacc70558f06ba6cd
Jungkoo Kang <jungkoo.kang@ibm.com>
2025-03-04 13:20:30 -0500

    Add evaluator example for superhero

--- tests/ali_integrated_llm/helpers/test_file_helper.py
+++ tests/ali_integrated_llm/helpers/test_file_helper.py
@@ -5,6 +5,7 @@ from api_integrated_llm.data_models.source_models import QuerySourceModel
 from api_integrated_llm.helpers.file_helper import (
     get_base_model_from_json,
     get_files_in_folder,
+    write_json,
 )
 
 
@@ -22,4 +23,40 @@ def test_parse_sources() -> None:
             file_path=file_path,
             base_model=QuerySourceModel,
         )
+    assert True
+
+
+def test_parse_sources_add_sample_id() -> None:
+    obj = get_base_model_from_json(
+        file_path=Path(
+            os.path.join(
+                project_root_path,
+                "tests",
+                "data",
+                "source",
+                "evaluation",
+                "sequencing_bird_superhero.json",
+            )
+        ),
+        base_model=QuerySourceModel,
+    )
+
+    obj.dataset = "superhero"
+    for i in range(len(obj.data)):
+        obj.data[i].sample_id = i + 1
+
+    write_json(
+        file_path=Path(
+            os.path.join(
+                project_root_path,
+                "tests",
+                "data",
+                "source",
+                "evaluation",
+                "sequencing_bird_superhero.json",
+            )
+        ),
+        base_model=obj,
+    )
+
     assert True
