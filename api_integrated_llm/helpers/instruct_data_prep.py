030afccb518aecee2e6ff7344e529d82418fe01e
Jungkoo Kang <jungkoo.kang@ibm.com>
2025-03-05 11:34:47 -0500

    Improve file handling

--- api_integrated_llm/helpers/instruct_data_prep.py
+++ api_integrated_llm/helpers/instruct_data_prep.py
@@ -59,7 +59,7 @@ def get_prompt_dict(
     path_str = str(evaluation_input_file_path)
     if "rest" in path_str:
         return prompt_dict_all["icl"]
-    if "sequencing" in path_str:
+    if "sequencing" in path_str or "slot_filling" in path_str:
         return prompt_dict_all["sequencing"]
     return prompt_dict_all["icl"]
 
