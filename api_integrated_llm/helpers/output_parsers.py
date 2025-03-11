c454d406a629652631964811fa199e3ba0bcda54
Jungkoo Kang <jungkoo.kang@ibm.com>
2025-03-11 15:44:45 -0400

    Use Two step json parser

--- api_integrated_llm/helpers/output_parsers.py
+++ api_integrated_llm/helpers/output_parsers.py
@@ -387,9 +387,8 @@ def parse_mistral_7b_instruct_v0_3(
         print(e)
         try:
             pred = prediction["generated_text"].strip()
-            pred_dict_list = get_json_data_with_two_step_parsing(
-                txt=pred.replace("\n", "").replace("\_", "_"),  # noqa: W605,
-                should_return_list=True,
+            pred_dict_list = json.loads(
+                pred.replace("\n", "").replace("\_", "_"),  # noqa: W605,
             )  # noqa: W605
             if skip_grounding:
                 pred_func_calls = [json.dumps(func) for func in pred_dict_list]
